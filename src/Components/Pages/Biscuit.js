f5dc30ce0b3dbab5067c6b436301c451db67bf0f
Claudia Miranda <clauh9@outlook.pt>
2023-03-14 15:58:50 +0000

    authentication working

--- src/Components/Pages/Biscuit.js
+++ src/Components/Pages/Biscuit.js
@@ -2,13 +2,21 @@ import React, { useState } from 'react';
 import ImageGrid from '../ImageGrid';
 import UploadForm from '../UploadForm';
 import Modal from '../Modal';
+import { useAuthContext } from '../AuthContext';
 
 
 const Biscuit = () => {
     const [selectedImg, setSelectedImg] = useState(null);
+    const { currentUser } = useAuthContext();
+    let compareID = "";
+    if (currentUser === null) {
+        compareID = "1";
+    } else {
+        compareID = currentUser.uid;
+    }
     return (
         <div>
-            <UploadForm folder={"Biscuit/"} collectionFolder={"biscuit"} />
+            {compareID === process.env.REACT_APP_USER_ADMIN && <UploadForm folder={"Biscuit/"} collectionFolder={"biscuit"} />}
             <ImageGrid setSelectedImg={setSelectedImg} collectionFolder={"biscuit"} />
             {selectedImg && (
                 <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
--- src/Components/Pages/Croche.js
+++ src/Components/Pages/Croche.js
@@ -2,12 +2,20 @@ import React, { useState } from 'react';
 import ImageGrid from '../ImageGrid';
 import UploadForm from '../UploadForm';
 import Modal from '../Modal';
+import { useAuthContext } from '../AuthContext';
 
 const Croche = () => {
     const [selectedImg, setSelectedImg] = useState(null);
+    const { currentUser } = useAuthContext();
+    let compareID = "";
+    if (currentUser === null) {
+        compareID = "1";
+    } else {
+        compareID = currentUser.uid;
+    }
     return (
         <div>
-            <UploadForm folder={"Croche/"} collectionFolder={"croche"} />
+            {compareID === process.env.REACT_APP_USER_ADMIN && <UploadForm folder={"Croche/"} collectionFolder={"croche"} />}
             <ImageGrid setSelectedImg={setSelectedImg} collectionFolder={"croche"} />
             {selectedImg && (
                 <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
